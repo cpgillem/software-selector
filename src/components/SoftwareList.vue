9daa4c542e40f2bdacac9d5c878cc03d72f05ffc
Cade Gillem <cade.p.gillem@wmich.edu>
2018-05-09 12:57:38 -0400

    Clear out default code.

---
+++ src/components/SoftwareList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="softwareList">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SoftwareList',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
